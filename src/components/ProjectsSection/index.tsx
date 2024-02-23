c27577a91692c31ad7d2bcce304724ffadd90c25
abhiramdesai17 <114446454+abhiramdesai17@users.noreply.github.com>
2024-02-22 20:45:05 -0500

    commit_15

--- src/components/ProjectsSection/index.tsx
+++ src/components/ProjectsSection/index.tsx
@@ -11,9 +11,9 @@ import PlayIcon from '@images/icons/play-icon.svg';
 import Image from "next/image";
 
 const TAGS = {
-  FRONTEND: "Frontend",
-  BACKEND: "Backend",
-  DATAENGINEER: "Data Engineering"
+  DS: "Data Science and ML",
+  DA: "Data Analysis and Visualization",
+  NN: "Neural Network"
 };
 
 const PER_PAGE_PROJECTS = 3;
@@ -38,85 +38,90 @@ const randomColors = [
 
 const mainProjects = [
   {
-    id: "assignments-tracker",
-    name: "Assignments Tracker",
-    description: `This Project is an attempt to learn to setup a easily reproducible and consistent infra on AWS for an small scale application like assignments submission portal considering every aspect of security, availability, reliability, scalability leveraging various cloud services.`,
-    techStack: ["Golang", "AWS", "IAM", "EC2", "S3", "VPC", "Subnets", "Internet Gateway", "Route Table", "Route53", "Lambdas", "Security Groups", "RDS", "Parameter Group", "Cloud Watch", "StasD", "Alerting", "Load Balancers", "Auto Scaler", "DNS", "Name Servers", "CAA Records", "SubDomains", "Key Vault", "Git Actions", "Packer", "SystemD", "Shell Script", "SSl Certificates", "Roles & Policies", "Amazon Simple Mailing Service"],
-    imgURL: "/images/assignment-submission.png",
-    githubLink: `https://github.com/Cloud-Computing-CSYE6225-Fall2023`,
-    tags: [TAGS.BACKEND],
+    id: "llm_model",
+    name: "Data Verification using LLM",
+    description: `Developed Large Language Model (LLM) using Optical Character Recognition (OCR) technology on various use cases – images containing text with different font, sentiment analysis, hand written notes to extract text from images. Leveraged transformers to utilize pre-trained LLM models like BART for various Natural Language Processing (NLP) tasks, enhancing accuracy of the extracted text output`,
+    techStack: ["Python", "Pandas", "Numpy", "OpenCV", "Tesseract", "Transformers", "Neural Networks"],
+    imgURL: "/images/llm.jpg",
+    githubLink: `https://github.com/abhiramdesai17/Data-Verification-LLM`,
+    tags: [TAGS.NN]
   },
+
   {
-    id: "audio-brief",
-    name: "Audio Brief",
-    description: `Designed data pipelines to process 5k ebooks from the dataset to extract each chapter and summarize
-chapters, which were then converted to audio books that are stored to google cloud buckets. Engineered the process of chapter extraction through Google Pub/Sub and Cloud Functions, resulting in
-optimized efficiency and enhanced reliability. Leveraged LLM with langchain and Google TTS as a Model as a Service to deliver on demand summaries and
-audio transcripts for selected chapters from a book. Enhanced application debugging by facilitating proper log messages and metrics aggregated using Cloud Logging.`,
-    techStack: ["Python", "FASTAPI", "Multi-Threading", "Airflow", "Pinecone", "GCP", "Cloud Storage", "Cloud SQL", "Google Pub/Sub", "Cloud Functions", "Google TTS", "Open AI", "RAG", "LangChain", "Terraform"],
-    imgURL: "/images/assignment01.png",
-    githubLink: `https://github.com/shivasaicharanruthala/AudioBrief`,
-    webLink: `https://codelabs-preview.appspot.com/?file_id=1tawz6aVeswcHqI2OKAxYyYzYdJ5Nxs-1t2lzuXzI5OU#0`,
-    tags: [TAGS.DATAENGINEER, TAGS.BACKEND],
+    id: "credit-card-default-classifier",
+    name: "Credit Card Default Classifier",
+    description: `Conducted comprehensive pre-processing and imputation on financial dataset to determine if client has defaulted in monthly credit card payments, interpreting findings through visualization and advanced statistical techniques.
+    Developed and evaluated various machine learning models including logistic regression, SVM and Random Forest classifier, identifying SVM as the optimal model with 86% classification accuracy, improving efficiency by 17% compared to baseline model.`,
+    techStack: ["Python", "Pandas", "Numpy", "Scikitlearn", "Tensorflow", "Seaborn", "Matplotlib"],
+    imgURL: "/images/creditcarddefault.jpg",
+    githubLink: `https://github.com/abhiramdesai17/Default-of-Credit-Card-Clients`,
+    tags: [TAGS.DS]
   },
+
   {
-    id: "semantic-search",
-    name: "Contextual Search",
-    description: `Developed a cutting-edge Contextual Search Application catering to analysts' needs. Leveraging vector similarity search and Redisearch with RAG, the application enables analysts to input queries and efficiently retrieve top-k statements from earnings call transcripts by ensuring a user-friendly and intuitive interface for enhanced usability.`,
-    techStack: ["Google compute engine", "Airflow", "Redisearch", "FastAPI", "Streamlit", "Docker", "Terraform", "OpenAI", "RAG"],
-    imgURL: "/images/assignment01.png",
-    githubLink: `https://github.com/BigDataIA-Summer2023-Team1/Assignment_01`,
-    webLink: `https://codelabs-preview.appspot.com/?file_id=1DyfWnAj3sUVshCtt96xWAaFhrE6ifEfT13ZruDm3dMc#0`,
-    demoLink: `https://codelabs-preview.appspot.com/?file_id=1DyfWnAj3sUVshCtt96xWAaFhrE6ifEfT13ZruDm3dMc#0`,
-    tags: [TAGS.DATAENGINEER],
+    id: "flight-delay-classifier",
+    name: "Flight Delay Classification Model",
+    description: `Implemented a flight classification model based on the flight delay factor employing a dataset encompassing flight data recorded in January 2019 in the United States consisting of 583,986 records and 22 binary categorical data attributes.
+    Performed intricate cleaning and preprocessing operations and performed exploratory data analysis (EDA) to identify
+    trends and make interpretations and optimizations on a logistic regression base model with classification accuracy of 85%.
+    Employed 5+ advanced machine learning algorithms with highest classification accuracy of 92% on Multi Layer Pereptron (MLP) Neural Network, improving data efficiency by 7%`,
+    techStack: ["Python", "Pandas", "Numpy", "Scikitlearn", "Seaborn", "Matplotlib"],
+    imgURL: "/images/flightdelay.jpg",
+    githubLink: `https://github.com/abhiramdesai17/flight-delay-classifier`,
+    tags: [TAGS.DS]
   },
+
+
   {
-    id: "visual-search",
-    name: "Visual Search",
-    description: `In this project, I harnessed the Azure Vision SDK (Florence) to conduct visual similarity searches using image vector embeddings, all prompted by user input. Employing Terraform, I established a highly reproducible Google Cloud Platform (GCP) infrastructure. Additionally, I containerized and deployed the model as a user-friendly service, allowing users to engage in tasks like background removal and image similarity searches via an intuitive UI. This endeavor highlights my proficiency in implementing cutting-edge technology and delivering seamless, reproducible solutions for real-world applications.`,
-    techStack: ["Google compute engine", "Streamlit", "Docker", "Terraform", "Azure Computer Vision"],
-    imgURL: "/images/assignment03.png",
-    githubLink: `https://github.com/BigDataIA-Summer2023-Team1/Assignment_03`,
-    webLink: `https://codelabs-preview.appspot.com/?file_id=12dn3Hs3BN9EUdLaln8LcXcGW6GwyTEJBKvV6ACbZrAk#0`,
-    tags: [TAGS.DATAENGINEER],
+    id: "covid19",
+    name: "Covid 19 Storyboard - Data Visualization",
+    description: `Developed a comprehensive Tableau Dashboard to analyze the global impact of the Covid-19 pandemic, utilizing data from January 2021 to October 2021.
+    Processed and curated a diverse set of data, encompassing cases, deaths, vaccinations, and other relevant metrics, sourced from countries worldwide.
+    Conducted in-depth trend analysis, identifying key patterns and anomalies within the dataset. Insights included the identification of countries with the highest number of cases, most deaths, and the highest vaccination and recovery rates.
+    Leveraged advanced analytical techniques to explore correlations between Covid-19 metrics and factors such as health-related issues, age demographics, and other relevant variables.
+    Contributed actionable insights to public health discussions by translating complex data patterns into understandable narratives for stakeholders.
+    Collaborated with cross-functional teams to ensure the accuracy and reliability of data sources, and facilitated ongoing updates to the dashboard for real-time monitoring.`,
+    techStack: ["Python", "Tableau"],
+    imgURL: "/images/covid19.jpg",
+    webLink: `https://public.tableau.com/app/profile/abhiram.desai/viz/COVID-19Storyboard_17069095125430/COVID-19StoryBoard`,
+    tags: [TAGS.DA]
   },
+
   {
-    id: "prep-buddy",
-    name: "PrepBuddy",
-    description: `Developed a web application customized to assist university students in preparing for interviews. By seamlessly integrating Google Drive and Calendar APIs, I enabled students to schedule mock interviews with their peers. The user-friendly UI, designed in React.js, ensured easy navigation while tracking mock interviews. To prioritize security, I implemented JWT authentication and password encryption, guaranteeing secure access to resources. This project demonstrates my ability to create practical and secure applications tailored to specific user needs.`,
-    techStack: ["React", "Node.js", "MongoDB", "Express", "ContextAPI", "Google calender API", "Google Drive API"],
-    imgURL: "/images/prepbuddy.png",
-    githubLink: `https://github.com/shivasaicharanruthala/prep-buddy-backend`,
-    webLink: `https://startling-cranachan-1e8cbc.netlify.app/`,
-    tags: [TAGS.BACKEND],
+    id: "crime-prediction",
+    name: "Crime Prediction ML Project",
+    description: `Designed linear regression, kNN, decision tree and random forest prediction models to predict the total number of offenses per million people based on locality and type of crime to assist police department of the city with accuracy of 87%.
+    Performed exploratory data analysis and generated geospatial visualization of the data showing the offence type with highest number of offences in that province.
+    Uploaded the cleaned dataset in the data warehouse (AWS Redshift) connected with an interactive Tableau dashboard.`,
+    techStack: ["Python", "Geopandas", "Redshift", "Tableau"],
+    imgURL: "/images/crimeprediction.png",
+    githubLink: `https://github.com/abhiramdesai17/Crime-Prediction`,
+    tags: [TAGS.DS]
   },
+
   {
-    id: "chef-it-out",
-    name: "Chef It Out",
-    description: `I conceptualized and developed a dynamic platform facilitating seamless connections between chefs, hotels, and consumers with just a few clicks. The platform also offers a subscription feature, allowing customers to conveniently schedule dishes. Additionally, I integrated Paytm's payment gateway to support both subscription-based payments and direct payments during checkout. This project showcases my ability to create innovative solutions that bridge culinary talents with eager audiences while ensuring efficient and secure transactions.`,
-    techStack: ["React", "Node.js", "MongoDB", "Express", "Redux"],
-    imgURL: "/images/chef-it-out.png",
-    githubLink: `https://github.com/shivasaicharanruthala/comfort-clothing`,
-    tags: [TAGS.BACKEND],
+    id: "uber-lyft",
+    name: "Uber vs Lyft Classification Analysis",
+    description: `Performed quantitative analysis of Uber and Lyft rides in Boston, MA to accurately determine which of the two is more likely to be used based on a set of given factors.
+    The dataset consists of 700k+ instances and 57 attributes and takes into consideration the prices of Uber and Lyft rides in and around the city of Boston. It includes various factors like the source and destination of the ride, time of the day, weather conditions, pricing, etc.`,
+    techStack: ["Pandas", "Numpy", "Matplotlib", "Seaborn", "Tableau", "Google Sites"],
+    imgURL: "/images/uberlyft.png",
+    webLink: `https://sites.google.com/husky.neu.edu/ubervslyft/home`,
+    tags: [TAGS.DA]
   },
+
   {
-    id: "crwn-clothing",
-    name: "Crwn Clothing",
-    description: `Built an E-Commerce application as an educational project to grasp the fundamentals of React.js, employing the Context API for efficient state management. This project allowed me to delve into component isolation and data flow design, providing valuable insights into building robust web applications with a focus on user interfaces and seamless data management..`,
-    techStack: ["React", "Node.js", "MongoDB", "Express", "Context API"],
-    imgURL: "/images/crown-clothing.png",
-    githubLink: `https://github.com/shivasaicharanruthala/comfort-clothing`,
-    tags: [TAGS.BACKEND],
-  },
-  {
-    id: "ice-breaker",
-    name: "IceBreaker",
-    description: `I designed and developed a robust crowdfunding website, streamlining campaign creation and management for users. By seamlessly integrating payment processing, I ensured secure contributions via various methods, including credit cards and PayPal. To enhance application reliability, I implemented Celery for email notifications and file uploads, enabling automated retries on failures. This project demonstrates my expertise in creating efficient and secure platforms that facilitate fundraising and user engagement.`,
-    techStack: ["Python", "Django", "ORM", "Celery", "SQL", "Bootstrap"],
-    imgURL: "/images/padhai.jpeg",
-    githubLink: ``,
-    tags: [TAGS.BACKEND],
-  },
+    id: "stroke-prediction",
+    name: "Stroke Prediction",
+    description: `Standardized regression models to predict whether a patient is likely to get a stroke based on gender, age, diseases and smoking status with a prediction accuracy of 90.2% on Naive Bayes model. 
+    Plotted 7 count plots to observe behaviour of stroke based on various categorical features like gender, hypertension, marital status, heart disease, residence, smoking status and employment.
+    Modelled kNN, SVM and Random Forest models to improve overall efficiency by 9.5% with kNN model giving highest accuracy of 99.7%.`,
+    techStack: ["Pandas", "Numpy", "Seaborn", "Matplotlib", "Scikitlearn"],
+    imgURL: "/images/strokeprediction.png",
+    githubLink: `https://github.com/abhiramdesai17/Stroke-Prediction`,
+    tags: [TAGS.DS]
+  }
+  
 ];
 
 function ProjectsSection() {
