bb24aae22c0ec7074337e64b85de6c9279519f9e
abhiramdesai17 <114446454+abhiramdesai17@users.noreply.github.com>
2024-03-04 15:08:36 -0500

    Update index.tsx

--- src/components/HeaderLogo/index.tsx
+++ src/components/HeaderLogo/index.tsx
@@ -8,7 +8,7 @@ function HeaderLogo() {
   return (
     <div className={classNames(sacramento.className, "flex gap-2 shrink-0")}>
       <AppText textTag="span" secondary customClass="sm:text-4xl text-3xl">
-        {"<"}
+        {""}
       </AppText>
       <a href="#">
         <AppText textTag="span" defaultColor customClass="sm:text-4xl text-3xl">
@@ -16,7 +16,7 @@ function HeaderLogo() {
         </AppText>
       </a>
       <AppText textTag="span" secondary customClass="sm:text-4xl text-3xl">
-        {"/>"}
+        {""}
       </AppText>
     </div>
   );
